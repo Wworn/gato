2c13121b636d60e43377921948f091f353f3a05c
Wworn <dedmause000@gmail.com>
2024-08-03 01:02:06 -0300

    Add files via upload

--- gato.sh
+++ gato.sh
@@ -13,24 +13,22 @@ cat_art2="
   > ^ <  
 "
 
-# Obtener el ancho de la terminal
+
 cols=$(tput cols)
-# Obtener el alto de la terminal
+
 rows=$(tput lines)
-# Número de líneas del arte ASCII del gato
+
 cat_height=3
 
-# Calcular el espacio superior e izquierdo para centrar el gato
 top_padding=$(( (rows - cat_height) / 2 ))
 left_padding=$(( (cols - 7) / 2 ))
 
-# Mostrar el gato "bailando" en el centro de la terminal
 while true
 do
     clear
-    # Mover el cursor a la posición de la parte superior del gato
+    
     tput cup $top_padding $left_padding
-    # Alternar entre las dos poses
+    
     if [[ $(( $(date +%s%N) / 100000000 % 2 )) -eq 0 ]]
     then
         echo "$cat_art1"
